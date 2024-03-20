2e0179d3024179536d596a29adbe604e9ea7ce40
DaviDio <david.cascella.5@gmail.com>
2024-03-20 13:45:55 +0100

    changes on pom client

---
+++ client/src/main/java/it/ji/game/client/Main.java
@@ -0,0 +1,23 @@
+package it.ji.game.client;
+
+import it.ji.game.client.exceptions.ServerNotFoundException;
+import it.ji.game.client.manager.ClientGameManager;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Starting client...");
+        System.out.println("insert server id: ");
+        String serverId = scanner.nextLine();
+        System.out.println("insert username: ");
+        String username = scanner.nextLine();
+        try {
+            ClientGameManager.getInstance().startClient(serverId, username);
+        } catch (ServerNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
