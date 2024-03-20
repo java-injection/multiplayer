d3f9b0d15a87e45dc2ebfa18986b98e1763b7048
Luca Coraci <luca.coraci@gmail.com>
2024-03-20 15:07:31 +0100

    ho sofferto assai

--- client/src/main/java/it/ji/game/client/Main.java
+++ client/src/main/java/it/ji/game/client/Main.java
@@ -1,6 +1,7 @@
 package it.ji.game.client;
 
 import it.ji.game.client.exceptions.ServerNotFoundException;
+import it.ji.game.client.gui.MainGui;
 import it.ji.game.client.manager.ClientGameManager;
 import it.ji.game.utils.settings.Settings;
 
@@ -8,17 +9,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Starting client...");
-        System.out.println("insert server id: ");
-        String serverId = scanner.nextLine();
-        System.out.println("insert username: ");
-        String username = scanner.nextLine();
         try {
-            ClientGameManager.getInstance().startClient(serverId, username);
-        } catch (ServerNotFoundException e) {
-            e.printStackTrace();
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainGui().setVisible(true);
+            }
+        });
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Starting client...");
+//        System.out.println("insert server id: ");
+//        String serverId = scanner.nextLine();
+//        System.out.println("insert username: ");
+//        String username = scanner.nextLine();
+//        try {
+//            ClientGameManager.getInstance().startClient(serverId, username);
+//        } catch (ServerNotFoundException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
